ae5f19b836c09a4308543cab47e21bf66356d1f0
yadavprynce <yadavprynce@gmail.com>
2026-09-02 17:42:11 +0530

    Added cd for staging

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,11 +1,10 @@
-import { client } from "@repo/db/client"
+import { client } from "@repo/db/client";
 
 export default async function Home() {
-
   const user = await client.user.findFirst();
 
   return (
-    <div >
+    <div>
       {user?.username}
       {user?.password}
     </div>
